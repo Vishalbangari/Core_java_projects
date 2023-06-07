65d624fecd97dae7378c6e7af8593234aaec0d98
vishal bangari <vishalbangari99@gmail.com>
2023-06-07 14:37:56 -0500

    Fizzbuzz program

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -2,7 +2,7 @@
 public class FibonacciSeries {
         public static void main(String[] args) {
 
-            int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
+            int i = 1, n = 20, firstTerm = 0, secondTerm = 1;
             System.out.println("Fibonacci Series till " + n + " terms:");
 
             while (i <= n) {
