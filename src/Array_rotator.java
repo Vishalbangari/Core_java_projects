407a0b17e63f733d6368bf7a54e8fc0217ea5088
vishal bangari <vishalbangari99@gmail.com>
2023-06-13 14:56:30 -0500

    Array rotator has been updated

--- src/Array_rotator.java
+++ src/Array_rotator.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Array_rotator {
     public static void main(String[] args) {
             int[] arr = {1, 2, 3, 4, 5, 6, 7};
-            int x = 2;
+            int x = 3;
 
             System.out.println("Original array: " + Arrays.toString(arr));
 
