21ca2b45c0cab228463619fb7889445febc24c20
vishal bangari <vishalbangari99@gmail.com>
2023-06-13 14:50:10 -0500

    Day 7 programs

---
+++ src/d06062023/Reverse_array.java
@@ -0,0 +1,15 @@
+package d06062023;
+import java.util.*;
+public class Reverse_array {
+        public static void main(String[] args){
+            int a[] = new int[]{2,3,4,5,6};
+            int n = a.length;
+
+            for(int i=n-1;i>=0;i--)
+            {
+                System.out.println(a[i]);
+            }
+
+
+        }
+    }
